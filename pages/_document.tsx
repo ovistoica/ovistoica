f2be31acb352f0a9b8222941cbe20c8e926439f2
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2022-02-18 17:51:43 +0200

    Remove useless

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,7 @@
 import * as React from 'react'
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from 'next/document'
-import {extractCritical} from '@emotion/server'
+import Document, {Html, Head, Main, NextScript} from 'next/document'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    const styles = extractCritical(initialProps.html)
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          <style
-            data-emotion-css={styles.ids.join(' ')}
-            dangerouslySetInnerHTML={{__html: styles.css}}
-          />
-        </>
-      ),
-    }
-  }
-
   render() {
     return (
       <Html>
